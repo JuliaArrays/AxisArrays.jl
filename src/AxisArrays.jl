cca9b8c998471f85bc92e56aabb94a085846f598
Matt Bauman <mbauman@gmail.com>
2015-02-18 19:06:56 -0500

    Refactor into three files

--- src/AxisArrays.jl
+++ src/AxisArrays.jl
@@ -3,5 +3,7 @@ module AxisArrays
 export AxisArray, Axis, Interval, axisnames
 
 include("core.jl")
+include("intervals.jl")
+include("indexing.jl")
 
 end
---
+++ src/intervals.jl
@@ -0,0 +1,43 @@
+@doc """
+A primitive interval type.
+
+### Type parameters
+
+```julia
+immutable Interval{T}
+```
+* `T` : the type of the interval
+
+### Constructors
+
+```julia
+Interval{T}(a::T,b::T)
+```
+
+### Arguments
+
+* `a` : lower bound of the interval
+* `b` : upper bound of the interval
+
+### Examples
+
+```julia
+A = AxisArray(collect(1:20), (.1:.1:2.0,), (:time,))
+A[Interval(0.0,0.5)] 
+A[Interval(0.2,0.5)] 
+```
+
+""" ->
+immutable Interval{T}
+    lo::T
+    hi::T
+    Interval(lo, hi) = lo <= hi ? new(lo, hi) : error("lo must be less than or equal to hi")
+end
+Interval{T}(a::T,b::T) = Interval{T}(a,b)
+Base.promote_rule{T,S}(::Type{Interval{T}}, ::Type{Interval{S}}) = Interval{promote_type(T,S)}
+Base.promote_rule{T,S}(::Type{Interval{T}}, ::Type{S}) = Interval{promote_type(T,S)}
+Base.convert{T,S}(::Type{Interval{T}}, x::Interval{S}) = (R = promote_type(T,S); Interval{R}(convert(R,x.lo),(convert(R,x.hi))))
+Base.convert{T}(::Type{Interval{T}}, x) = Interval(x,x)
+Base.isless(a::Interval, b::Interval) = isless(a.hi, b.lo)
+Base.isless(a::Interval, b) = isless(promote(a,b)...)
+Base.isless(a, b::Interval) = isless(promote(a,b)...)
