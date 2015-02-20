21f2156bcd49c526bd1d016ec13265ead0856054
Tom Short <tshort.rlists@gmail.com>
2015-02-20 09:57:08 -0500

    Add optional utilities for conversion to a DataFrame and for plotting with Gadfly.
    Note that this adds a dependency to the Requires package.

--- src/AxisArrays.jl
+++ src/AxisArrays.jl
@@ -1,9 +1,12 @@
 module AxisArrays
 
+using Requires
+
 export AxisArray, Axis, Interval, axisnames
 
 include("core.jl")
 include("intervals.jl")
 include("indexing.jl")
+include("utils.jl")
 
 end
