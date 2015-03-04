803285bad4fe9cb2dd7f7c5273c8686c1cdf66e7
milktrader <milktrader@gmail.com>
2015-03-04 08:09:56 -0500

    refactor moving method

--- src/timeseries.jl
+++ src/timeseries.jl
@@ -3,14 +3,32 @@
 import TimeSeries.moving, Base.merge
 
 # only works for single column AxisArrays
+# TODO generalize to work over any dimension (see commented prototype code below
+
 function moving(a::AxisArray, f::Function, n::Int)
     m = length(a)-n+1
     res = zeros(m)
     for i in 1:m
-        res[i] = f(a[Interval(a.axes[1][i],a.axes[1][i+n-1])])
+        #res[i] = f(a[Interval(a.axes[1][i],a.axes[1][i+n-1])])
+        res[i] = f(a[Axis{1}(i:i+n-1)])
     end
     AxisArray(res, (a.axes[1][n:end],))
 end
+
+# generalized moving over any dimension
+# to be called thus: moving(AAPL, mean, Axis{:time}(10))
+##### NOT WORKING ### function moving{Ax<:Axis}(a::AxisArray, f::Function, n::Ax)
+##### NOT WORKING ###     v = n.val
+##### NOT WORKING ###     m = size(a, n) - v
+##### NOT WORKING ###     res = similar(a, (Ax(m),), (axes(a, Ax)[cld(v,2):end-fld(v,2)],))
+##### NOT WORKING ###     for i in 1:m
+##### NOT WORKING ###         #res[Ax(i)] = f(a[Ax[i:i+v-1])
+##### NOT WORKING ###         res[Ax(i)] = f(a[Ax[i:i+v-1]])
+##### NOT WORKING ###     end
+##### NOT WORKING ### end
+
+
+
 
 # this is @tshort implementation with correction to overlaps
 
