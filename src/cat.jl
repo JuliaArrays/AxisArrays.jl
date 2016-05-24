d5b9d24d743916af432b68df3ddf497b097ccd93
Gord Stephen <gord@gordstephen.ca>
2016-05-24 16:23:22 -0400

    Changed function syntax for Base.cat

--- src/cat.jl
+++ src/cat.jl
@@ -13,12 +13,14 @@ sizes{T<:AxisArray}(As::T...) = tuple(zip(map(size, As)...)...)
 matchingdims{N,T<:AxisArray}(As::NTuple{N,T}) = all(equalvalued, sizes(As...))
 matchingdimsexcept{N,T<:AxisArray}(As::NTuple{N,T}, n::Int) = all(equalvalued, sizes(As[[1:n-1; n+1:end]]...))
 
-Base.cat{T<:AxisArray}(n::Int, As::T...) = if n <= ndims(As[1])
-    matchingdimsexcept(As, n) || error("All non-concatenated axes must be identically-valued")
-    newaxis = Axis{axisnames(As[1])[n]}(vcat(map(A -> A.axes[n].val, As)...))
-    checkaxis(newaxis)
-    return AxisArray(cat(n, map(A->A.data, As)...), (As[1].axes[1:n-1]..., newaxis, As[1].axes[n+1:end]...))
-else
-    matchingdims(As) || error("All axes must be identically-valued")
-    return AxisArray(cat(n, map(A->A.data, As)...), As[1].axes)
-end #if
+function Base.cat{T<:AxisArray}(n::Int, As::T...)
+    if n <= ndims(As[1])
+        matchingdimsexcept(As, n) || error("All non-concatenated axes must be identically-valued")
+        newaxis = Axis{axisnames(As[1])[n]}(vcat(map(A -> A.axes[n].val, As)...))
+        checkaxis(newaxis)
+        return AxisArray(cat(n, map(A->A.data, As)...), (As[1].axes[1:n-1]..., newaxis, As[1].axes[n+1:end]...))
+    else
+        matchingdims(As) || error("All axes must be identically-valued")
+        return AxisArray(cat(n, map(A->A.data, As)...), As[1].axes)
+    end #if
+end #Base.cat
