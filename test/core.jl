9813acaf2ce6e1b9e733ce953bf0bfac1cd32b83
Matt Bauman <mbauman@gmail.com>
2017-04-30 14:16:37 -0500

    Add tests

--- test/core.jl
+++ test/core.jl
@@ -1,4 +1,4 @@
-A = AxisArray(reshape(1:24, 2,3,4), .1:.1:.2, .1:.1:.3, .1:.1:.4)
+A = @inferred(AxisArray(reshape(1:24, 2,3,4), .1:.1:.2, .1:.1:.3, .1:.1:.4))
 @test_throws ArgumentError AxisArray(reshape(1:24, 2,3,4), .1:.1:.1, .1:.1:.3, .1:.1:.4)
 @test_throws ArgumentError AxisArray(reshape(1:24, 2,3,4), .1:.1:.1, .1:.1:.3)
 @test parent(A) === reshape(1:24, 2,3,4)
@@ -40,7 +40,7 @@ for perm in ((:col, :row, :page), (:col, :page, :row),
 end
 @test axisnames(permutedims(A, (:col,)))  == (:col, :row, :page)
 @test axisnames(permutedims(A, (:page,))) == (:page, :row, :col)
-A2 = AxisArray(reshape(1:15, 3, 5))
+A2 = @inferred(AxisArray(reshape(1:15, 3, 5)))
 A1 = AxisArray(1:5, :t)
 for f in (transpose, ctranspose)
     @test f(A2).data == f(A2.data)
@@ -100,12 +100,12 @@ A = AxisArray([1 3; 2 4], :a)
 VERSION >= v"0.5.0-dev" && @inferred(axisnames(A))
 @test axisvalues(A) == (1:2, 1:2)
 # Just axis values
-A = AxisArray(1:3, .1:.1:.3)
+A = @inferred(AxisArray(1:3, .1:.1:.3))
 @test A.data == 1:3
 @test axisnames(A) == (:row,)
 VERSION >= v"0.5.0-dev" && @inferred(axisnames(A))
 @test axisvalues(A) == (.1:.1:.3,)
-A = AxisArray(reshape(1:16, 2,2,2,2), .5:.5:1)
+A = @inferred(AxisArray(reshape(1:16, 2,2,2,2), .5:.5:1))
 @test A.data == reshape(1:16, 2,2,2,2)
 @test axisnames(A) == (:row,:col,:page,:dim_4)
 VERSION >= v"0.5.0-dev" && @inferred(axisnames(A))
@@ -135,10 +135,10 @@ B = AxisArray([1 4; 2 5; 3 6], (:x, :y), (0.2, 100), (-3,14))
                                      Axis{2}(1//10:1//10:3//10),
                                      Axis{3}(["a", "b", "c", "d"])) # Axis need to be symbols
 
-A = AxisArray(reshape(1:24, 2,3,4),
+A = @inferred(AxisArray(reshape(1:24, 2,3,4),
               Axis{:x}(.1:.1:.2),
               Axis{:y}(1//10:1//10:3//10),
-              Axis{:z}(["a", "b", "c", "d"]))
+              Axis{:z}(["a", "b", "c", "d"])))
 
 @test axisdim(A, Axis{:x}) == axisdim(A, Axis{:x}()) == 1
 @test axisdim(A, Axis{:y}) == axisdim(A, Axis{:y}()) == 2
@@ -176,7 +176,7 @@ T = A[AxisArrays.Axis{:x}]
 
 # Test Timetype axis construction
 dt, vals = DateTime(2010, 1, 2, 3, 40), randn(5,2)
-A = AxisArray(vals, Axis{:Timestamp}(dt-Dates.Hour(2):Dates.Hour(1):dt+Dates.Hour(2)), Axis{:Cols}([:A, :B]))
+A = @inferred(AxisArray(vals, Axis{:Timestamp}(dt-Dates.Hour(2):Dates.Hour(1):dt+Dates.Hour(2)), Axis{:Cols}([:A, :B])))
 @test A[:, :A].data == vals[:, 1]
 @test A[dt, :].data == vals[3, :]
 
@@ -229,7 +229,7 @@ map!(*, A2, A, A)
 
 # Reductions (issue #55)
 A = AxisArray(collect(reshape(1:15,3,5)), :y, :x)
-B = AxisArray(collect(reshape(1:15,3,5)), Axis{:y}(0.1:0.1:0.3), Axis{:x}(10:10:50))
+B = @inferred(AxisArray(collect(reshape(1:15,3,5)), Axis{:y}(0.1:0.1:0.3), Axis{:x}(10:10:50)))
 for C in (A, B)
     for op in (sum, minimum)  # together, cover both reduced_indices and reduced_indices0
         axv = axisvalues(C)
