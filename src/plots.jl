757273863f3335aca12506184815b2dc3e9c7721
Jan Weidner <jw3126@gmail.com>
2017-07-31 13:16:27 +0200

    fix

--- src/plots.jl
+++ src/plots.jl
@@ -2,13 +2,15 @@ using RecipesBase
 
 @recipe function plot{T}(arr::AxisArray{T, 1})
     xlabel --> axisnames(arr)[1]
-    xticks --> axisvalues(arr)[1]
-    arr.data
+    xticks --> (eachindex(arr.data), axisvalues(arr)[1])
+    xrotation --> -45
+    eachindex(arr.data), arr.data
 end
 
 @recipe function plot{T}(arr::AxisArray{T, 2})
     xlbl, ylbl = axisnames(arr)
     xlabel --> xlbl
     ylabel --> ylbl
+    xrotation --> -45
     axisvalues(arr)..., arr.data
 end
