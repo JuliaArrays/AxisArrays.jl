343d9ee01652bdd5f24f38a2295a679a09fa0eb4
Matt Bauman <mbauman@gmail.com>
2018-08-17 14:43:37 -0500

    Re-enable disabled tests - just don't test at-inferred

--- test/core.jl
+++ test/core.jl
@@ -327,14 +327,14 @@ for op in functions  # together, cover both reduced_indices and reduced_indices0
         @test C2 == reshape([1,2,3], 3, 1)
         @test C12 == reshape([1], 1, 1)
     end
-    # @test @inferred(op(C, dims=Axis{:y})) == C1
-    # @test @inferred(op(C, dims=Axis{:x})) == C2
-    # # Unfortunately the type of (Axis{:y},Axis{:x}) is Tuple{UnionAll,UnionAll} so methods will not specialize
-    # @test_broken @inferred(op(C, dims=(Axis{:y},Axis{:x}))) == C12
-    # @test op(C, dims=(Axis{:y},Axis{:x})) == C12
-    # @test @inferred(op(C, dims=Axis{:y}())) == C1
-    # @test @inferred(op(C, dims=Axis{:x}())) == C2
-    # @test @inferred(op(C, dims=(Axis{:y}(),Axis{:x}()))) == C12
+    # TODO: These should be @inferred, but are currently broken
+    @test (op(C, dims=Axis{:y})) == C1
+    @test (op(C, dims=Axis{:x})) == C2
+    # Unfortunately the type of (Axis{:y},Axis{:x}) is Tuple{UnionAll,UnionAll} so methods will not specialize
+    @test (op(C, dims=(Axis{:y},Axis{:x}))) == C12
+    @test (op(C, dims=Axis{:y}())) == C1
+    @test (op(C, dims=Axis{:x}())) == C2
+    @test (op(C, dims=(Axis{:y}(),Axis{:x}()))) == C12
 end
 
 C = AxisArray(collect(reshape(1:15,3,5)), Axis{:y}([:a,:b,:c]), Axis{:x}(["a","b","c","d","e"]))
