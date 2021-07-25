4e294723adfbbdab1f19aaf136b7ea33f6c73700
Jeff Fessler <JeffFessler@users.noreply.github.com>
2021-07-24 22:04:28 -0400

    Fix deprecations with nt.names and kw.data (#201)
    also fix a test-only depwarn using IntervalSets

--- src/core.jl
+++ src/core.jl
@@ -240,9 +240,9 @@ function AxisArray(A::AbstractArray{T,N}, names::NTuple{N,Symbol}, steps::NTuple
 end
 
 # Alternative constructor, takes names as keywords:
-AxisArray(A; kw...) = AxisArray(A, nt_to_axes(kw.data)...)
-@generated nt_to_axes(nt::NamedTuple) =
-    Expr(:tuple, (:(Axis{$(QuoteNode(n))}(getfield(nt, $(QuoteNode(n))))) for n in nt.names)...)
+AxisArray(A; kw...) = AxisArray(A, nt_to_axes(values(kw)))
+@generated nt_to_axes(nt::NamedTuple{names}) where {names} =
+    Expr(:tuple, (:(Axis{$(QuoteNode(n))}(getfield(nt, $(QuoteNode(n))))) for n in names)...)
 
 AxisArray(A::AxisArray) = A
 AxisArray(A::AxisArray, ax::Vararg{Axis, N}) where N =
