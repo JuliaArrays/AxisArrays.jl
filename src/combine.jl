537e733a60527fe4579d7d56ead803fa22596dbc
abhro <5664668+abhro@users.noreply.github.com>
2025-05-02 17:06:03 -0400

    Fix doctests

--- src/combine.jl
+++ src/combine.jl
@@ -221,8 +221,8 @@ axis of type `Axis{:collapsed, CategoricalVector{Tuple}}`.
 ```julia-repl
 julia> price_data = AxisArray(rand(10), Axis{:time}(Date(2016,01,01):Day(1):Date(2016,01,10)))
 1-dimensional AxisArray{Float64,1,...} with axes:
-    :time, 2016-01-01:1 day:2016-01-10
-And data, a 10-element Array{Float64,1}:
+    :time, Date("2016-01-01"):Day(1):Date("2016-01-10")
+And data, a 10-element Vector{Float64}:
  0.885014
  0.418562
  0.609344
@@ -236,9 +236,9 @@ And data, a 10-element Array{Float64,1}:
 
 julia> size_data = AxisArray(rand(10,2), Axis{:time}(Date(2016,01,01):Day(1):Date(2016,01,10)), Axis{:measure}([:area, :volume]))
 2-dimensional AxisArray{Float64,2,...} with axes:
-    :time, 2016-01-01:1 day:2016-01-10
-    :measure, Symbol[:area, :volume]
-And data, a 10×2 Array{Float64,2}:
+    :time, Date("2016-01-01"):Day(1):Date("2016-01-10")
+    :measure, [:area, :volume]
+And data, a 10×2 Matrix{Float64}:
  0.159434     0.456992
  0.344521     0.374623
  0.522077     0.313256
@@ -252,9 +252,9 @@ And data, a 10×2 Array{Float64,2}:
 
 julia> collapsed = collapse(Val(1), (:price, :size), price_data, size_data)
 2-dimensional AxisArray{Float64,2,...} with axes:
-    :time, 2016-01-01:1 day:2016-01-10
+    :time, Date("2016-01-01"):Day(1):Date("2016-01-10")
     :collapsed, Tuple{Symbol,Vararg{Symbol,N} where N}[(:price,), (:size, :area), (:size, :volume)]
-And data, a 10×3 Array{Float64,2}:
+And data, a 10×3 Matrix{Float64}:
  0.885014  0.159434     0.456992
  0.418562  0.344521     0.374623
  0.609344  0.522077     0.313256
